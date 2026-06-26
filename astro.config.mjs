6452139df89afc793e2d8ab454af1628d6eb75fd
HeronNepomuceno <heronnepo1@gmail.com>
2026-06-25 22:30:54 -0300

    feat: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,4 +16,5 @@ export default defineConfig({
 	vite: {
     plugins: [tailwindcss()],
   },
+  adapter: netlify(),
 });
